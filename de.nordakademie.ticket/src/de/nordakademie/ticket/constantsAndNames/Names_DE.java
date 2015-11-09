a9e914c0756c722a0a7bc22546fb4120004c367d
Felix <felifrank91@web.de>
2015-11-09 22:22:35 +0100

    Mehrsprachigkeit

---
+++ de.nordakademie.ticket/src/de/nordakademie/ticket/constantsAndNames/Names_DE.java
@@ -0,0 +1,114 @@
+package de.nordakademie.ticket.constantsAndNames;
+
+public interface Names_DE {
+	
+//Messages
+	public static String M_ADD_ROLE_TRANSITION_LONG = "Transition hinzufügen zu ";
+	public static String M_ADD_ROLE_TRANSITION_SHORT = "Transition hinzufügen";
+	public static String M_ALLOW_NEW_ISSUE_LONG = "Zum Anlegen von neuen Tickets berechtigen für ";
+	public static String M_ALLOW_NEW_ISSUE_SHORT = "Zum Anlegen von neuen Tickets berechtigen";
+	
+	public static String M_CREATE_RULE = "Erzeugen ";
+	public static String M_CHANGE_TRANSITION_LONG = "Transition ändern von ";
+	public static String M_CHANGE_TRANSITION_SHORT = "Transition ändern";
+	
+	public static String M_DAY_TO_FIRST_LONG_1 = "Anpassen des Tages von ";
+	public static String M_DAY_TO_FIRST_LONG_2 = " zum 1. des Monats";
+	public static String M_DAY_TO_FIRST_SHORT = "Anpassen des Tages auf den 1. des Monats";
+	
+	public static String M_EMPTY_DESCRIPTION = "Bezeichnung darf nicht leer sein";
+	public static String M_EMPTY_ENTRIES = "Einträge dürfen nicht leer sein";
+	public static String M_EMPTY_NAME = "Name darf nicht leer sein";
+	public static String M_EMPTY_TITLE = "Titel darf nicht leer sein";
+	
+	public static String M_FILL_EMPTY_ELEMENT_1 = "Füllen der leeren Komponente ";
+	public static String M_FILL_EMPTY_ELEMENT_2 = " mit ";
+	
+	public static String M_INVALID_DAY = "Bitte einen gültigen Tag eingeben";
+	public static String M_INVALID_MAIL = "Bitte eine gültige Mail eingeben ('example@gmx.com')";
+	public static String M_INVALID_MONTH = "Bitte einen gültigen Monat eingeben";
+	public static String M_INVALID_YEAR = "Bitte ein gültiges Jahr eingeben";
+	
+	public static String M_MONTH_TO_LONG_1 = "Ändern des Monats von ";
+	public static String M_MONTH_TO_LONG_2 = " zu ";
+	public static String M_MONTH_TO_SHORT = "Ändern des Monats zu ";
+	
+	public static String M_NEW_RULE_LONG_1 = "Erzeugen von neuem ";
+	public static String M_NEW_RULE_LONG_2 = " am Ende des Dokuments";
+	public static String M_NEW_RULE_SHORT = "Erzeuge neue/n/s ";
+	public static String M_NO_ROLE_TRANSITIONS = "Role muss zumindest eine Transition beinhalten";
+	public static String M_NO_COMBO_ENTRIES = "ComboField zumindest einen Eintrag besitzen";
+
+	public static String M_REMOVE_RULE_SHORT = "Entferne ";
+	public static String M_RENAME_RULE_SUFFIX = "_2";
+	public static String M_RENAME_RULE_LONG_1 = "Umbenennen von ";
+	public static String M_RENAME_RULE_LONG_2 = " zu ";
+	public static String M_RENAME_RULE_SHORT = "Umbenennen zu ";
+	
+	
+	public static String M_REMOVE_DUPLICATES_LONG_1 = "Alle doppelten";
+	public static String M_REMOVE_DUPLICATES_LONG_2 = " entfernen von ";
+	public static String M_REMOVE_DUPLICATES_SHORT = "Alle Duplicate entfernen";
+	public static String M_RULE_NEEDED_1 = "Zumindest 1 ";
+	public static String M_RULE_NEEDED_2 = " wird benötigt";
+	
+	public static String M_SAME_BEGIN_AND_END_STATUS = "Beginn-Status und End-Status müssen unterschiedlich sein";
+	public static String M_SAME_COMBO_ENTRIES = "ComboField muss unterschiedliche Einträge enthalten";
+	public static String M_SAME_ISSUE_SCREEN_FIELDS = "IssueScreen muss unterschiedliche Fields beinhalten";
+	public static String M_SAME_ISSUE_TYPE_FIELDS = "IssueType muss unterschiedliche Fields beinhalten";
+	public static String M_SAME_NAME = " bereits vorhanden";
+	public static String M_SAME_PERSON_ROLES = "Eine Person muss unterschiedliche Rollen beinhalten";
+	public static String M_SAME_ROLE_TRANSITIONS = "Eine Role muss unterschiedliche Transitions beinhalten";
+	public static String M_SAME_WORKFLOW_TRANSITIONS = "Ein Workflow muss unterschiedliche Transitions beinhalten";
+	
+	public static String M_TRANSITION_HAS_DIFFERENT_STATUS = "Beginn-Status und End-Status müssen unterschiedlich sein";
+
+	public static String M_YEAR_TO_LONG_1 = "Ändern des Jahres von ";
+	public static String M_YEAR_TO_LONG_2 = " zu ";
+	public static String M_YEAR_TO_SHORT = "Ändern des Jahres auf ";
+
+	
+// shown expressions
+	public static String S_ALL_ENTRIES = "alle Einträge";
+	public static String S_APRIL = "April";
+	public static String S_AUGUST = "August";
+
+	public static String S_DECEMBER = "Dezember";
+	public static String S_DESCRIPTION = "Bezeichnung";
+	
+	public static String S_ENTRY = "Eintrag";
+	public static String S_ENTRIES = "Einträge";
+	
+	public static String S_FEBRUARY = "Februar";
+	public static String S_FIELD = "Field";
+	public static String S_FIELDS = "Fields";
+	
+	public static String S_ISSUE_TYPE = "IssueType";
+	
+	public static String S_JANUARY = "Januar";
+	public static String S_JULY = "Juli";
+	public static String S_JUNE = "Juni";
+	
+	public static String S_MAY = "Mai";
+	public static String S_MARCH = "März";
+	
+	public static String S_NAME = "Name";
+	public static String S_NOVEMBER = "November";
+	
+	public static String S_OCTOBER = "Oktober";
+	
+	public static String S_PERSON = "Person";
+	
+	public static String S_ROLE = "Role";
+	public static String S_ROLES = "Roles";
+	
+	public static String S_SEPTEMBER = "September";
+	public static String S_STATUS = "Status";
+	
+	public static String S_TITLE = "Title";
+	public static String S_TO = "zu";
+	public static String S_TRANSITION = "Transition";
+	public static String S_TRANSITIONS = "Transitions";
+	
+	public static String S_WORKFLOW = "Workflow";
+}
